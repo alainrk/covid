5c8b9867f0a76470b5e56e1262aba1dbaaff3a64
Alain Di Chiappari <alain.devcs@gmail.com>
2020-03-22 15:57:34 +0100

    region data divide

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import numpy as np
 import scipy as misc
+import pandas as pd
+
+regions = ["Abruzzo", "Basilicata", "P.A. Bolzano", "Calabria", "Campania", "Emilia Romagna", "Friuli Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte", "Puglia", "Sardegna", "Sicilia", "Toscana", "P.A. Trento", "Umbria", "Valle d'Aosta", "Veneto"]
+
+# df = pd.read_csv('data.csv', sep=',' , header=None)
+df = pd.read_csv('data.csv')
+regions_data = {}
+
+for region in regions:
+  regions_data[region] = df[df["denominazione_regione"] == region]
+
+print(regions_data)
