9b00fa39ef655ccf7a4a24536234643ecafb57f2
Alain Di Chiappari <alain.devcs@gmail.com>
2020-03-24 06:20:33 +0100

    data

--- download.sh
+++ download.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-20200320.csv | head -1 > data.csv
 
 for i in $(seq 24 29); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202002$i.csv | tail -n +2 >> data.csv; done
 
 for i in $(seq 1 9); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-2020030$i.csv | tail -n +2 >> data.csv; done
 
-for i in $(seq 10 22); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202003$i.csv | tail -n +2 >> data.csv; done
+for i in $(seq 10 31); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202003$i.csv | tail -n +2 >> data.csv; done
