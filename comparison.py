8e3eac688b76c390d1bed81a458bf34d6af8f026
Alain Di Chiappari <alain.devcs@gmail.com>
2020-03-25 15:59:55 +0100

    delta calc

--- comparison.py
+++ comparison.py
@@ -21,7 +21,6 @@ def printRegions(regions, regions_data):
 # Useful columns
 # data,denominazione_regione,ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
 
-# df = pd.read_csv("data.csv", sep="," , header=None)
 df = pd.read_csv("data.csv")
 df = df.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti"])
 df["day"] = -1
@@ -32,17 +31,12 @@ df["data"] = df["data"].apply(convertData)
 df = df[df["totale_casi"] > 99]
 df = df[df["denominazione_regione"].isin(FILTERED_REGIONS)]
 
-# df.plot(x = "data", y = "totale_casi", kind = "scatter")
-# plt.show()
-
 fig, axs = plt.subplots(2, 2)
 
 regions_data = {}
 regions_list = []
 for region in FILTERED_REGIONS:
   regions_data[region] = df[df["denominazione_regione"] == region]
-  # tmp = df[df["denominazione_regione"] == region]
-  # regions_data[region] = tmp.copy()
   regions_list.append(regions_data[region])
 
   count = 0
@@ -50,7 +44,6 @@ for region in FILTERED_REGIONS:
     regions_data[region].at[index, "day"] = count
     count += 1
 
-  # regions_data[region].plot(x = "day", y = "totale_casi")
   axs[0, 0].plot("day", "totale_casi", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
   axs[0, 1].plot("day", "terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
   axs[1, 0].plot("day", "nuovi_attualmente_positivi", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import random
 
 # FILTERED_REGIONS = ["Abruzzo", "Basilicata", "P.A. Bolzano", "Calabria", "Campania", "Emilia Romagna", "Friuli Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte", "Puglia", "Sardegna", "Sicilia", "Toscana", "P.A. Trento", "Umbria", "Valle d'Aosta", "Veneto"]
-FILTERED_REGIONS = ["Emilia Romagna", "Lazio", "Lombardia", "Piemonte", "Puglia", "Toscana", "Veneto"]
+FILTERED_REGIONS = ["Emilia Romagna", "Lombardia", "Piemonte", "Veneto"]
+# FILTERED_REGIONS = ["Lombardia", "Veneto"]
 COLORS = { "Emilia Romagna": "green", "Lazio": "red", "Lombardia": "blue", "Piemonte": "brown", "Puglia": "orange", "Toscana": "purple", "Veneto": "black" }
 
 def randColor():
@@ -21,7 +22,6 @@ def printRegions(regions, regions_data):
 # Useful columns
 # data,denominazione_regione,ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
 
-# df = pd.read_csv("data.csv", sep="," , header=None)
 df = pd.read_csv("data.csv")
 df = df.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti"])
 df["day"] = -1
@@ -32,35 +32,51 @@ df["data"] = df["data"].apply(convertData)
 df = df[df["totale_casi"] > 99]
 df = df[df["denominazione_regione"].isin(FILTERED_REGIONS)]
 
-# df.plot(x = "data", y = "totale_casi", kind = "scatter")
-# plt.show()
-
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots(3, 2)
 
 regions_data = {}
 regions_list = []
 for region in FILTERED_REGIONS:
-  regions_data[region] = df[df["denominazione_regione"] == region]
-  # tmp = df[df["denominazione_regione"] == region]
-  # regions_data[region] = tmp.copy()
+  regions_data[region] = df[df["denominazione_regione"] == region].copy(True)
+  # regions_data[region].index = range(regions_data[region].size)
+
+  regions_data[region]["totale_casi_ieri"] = regions_data[region]["totale_casi"].shift(1)
+  regions_data[region]["delta_totale"] = (regions_data[region]["totale_casi"] - regions_data[region]["totale_casi_ieri"]) / regions_data[region]["totale_casi_ieri"] * 100
+
+  regions_data[region]["terapia_intensiva_ieri"] = regions_data[region]["terapia_intensiva"].shift(1)
+  regions_data[region]["delta_terapia_intensiva"] = (regions_data[region]["terapia_intensiva"] - regions_data[region]["terapia_intensiva_ieri"]) / regions_data[region]["terapia_intensiva_ieri"] * 100
+
+  regions_data[region]["deceduti_ieri"] = regions_data[region]["deceduti"].shift(1)
+  regions_data[region]["delta_deceduti"] = (regions_data[region]["deceduti"] - regions_data[region]["deceduti_ieri"]) / regions_data[region]["deceduti_ieri"] * 100
+
   regions_list.append(regions_data[region])
 
+  # Manual reindex
   count = 0
   for index, row in regions_data[region].iterrows():
     regions_data[region].at[index, "day"] = count
     count += 1
 
-  # regions_data[region].plot(x = "day", y = "totale_casi")
   axs[0, 0].plot("day", "totale_casi", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
-  axs[0, 1].plot("day", "terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
-  axs[1, 0].plot("day", "nuovi_attualmente_positivi", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
-  axs[1, 1].plot("day", "deceduti", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+  axs[0, 1].plot("day", "delta_totale", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+
+  axs[1, 0].plot("day", "terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+  axs[1, 1].plot("day", "delta_terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+
+  axs[2, 0].plot("day", "deceduti", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+  axs[2, 1].plot("day", "delta_deceduti", label=region, data=regions_data[region], markersize=2, color=COLORS[region], linewidth=2)
+
 
 axs[0, 0].set_title('Totale casi')
-axs[0, 1].set_title('Terapia intensiva')
-axs[1, 0].set_title('Nuovi casi')
-axs[1, 1].set_title('Deceduti')
-axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0, -0.1), ncol=5, fancybox=True, shadow=True)
+axs[0, 1].set_title('Δ casi giornalieri (%)')
+
+axs[1, 0].set_title('Terapia intensiva')
+axs[1, 1].set_title('Δ TI giornalieri (%)')
+
+axs[2, 0].set_title('Deceduti')
+axs[2, 1].set_title('Δ deceduti (%)')
+
+axs[2, 1].legend(loc='upper center', bbox_to_anchor=(0, -0.2), ncol=5, fancybox=True, shadow=True)
 
 plt.show()
 
