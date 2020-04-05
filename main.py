cb1ce26df4758947aa51e8158b270240fe3f379e
Alain Di Chiappari <alain.devcs@gmail.com>
2020-04-05 07:28:23 +0200

    Data

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import random
 from datetime import datetime
 from scipy.optimize import curve_fit
 
+# ONLY_SINGLE_ONE = True
+ONLY_SINGLE_ONE = False
+
 # FILTERED_REGIONS = ["Abruzzo", "Basilicata", "P.A. Bolzano", "Calabria", "Campania", "Emilia Romagna", "Friuli Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte", "Puglia", "Sardegna", "Sicilia", "Toscana", "P.A. Trento", "Umbria", "Valle d'Aosta", "Veneto"]
-# FILTERED_REGIONS = ["Italia"]
-FILTERED_REGIONS = ["Italia", "Emilia Romagna", "Lombardia", "Piemonte", "Veneto"]
+
+if ONLY_SINGLE_ONE:
+  # FILTERED_REGIONS = ["Italia"]
+  FILTERED_REGIONS = ["Lombardia"]
+else:
+  FILTERED_REGIONS = ["Italia", "Emilia Romagna", "Lombardia", "Piemonte", "Veneto"]
 
 COLORS = { "Emilia Romagna": "green", "Lazio": "magenta", "Lombardia": "blue", "Piemonte": "red", "Puglia": "pink", "Toscana": "purple", "Veneto": "orange", "Italia": "black" }
 
@@ -39,12 +46,12 @@ setColors()
 # data,denominazione_regione,ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
 
 df_it = pd.read_csv("italia.csv")
-df_it = df_it.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti", "totale_attualmente_positivi"])
+df_it = df_it.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti", "totale_attualmente_positivi", "tamponi"])
 df_it["day"] = -1
 df_it["denominazione_regione"] = "Italia"
 
 df_reg = pd.read_csv("regioni.csv")
-df_reg = df_reg.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti", "totale_attualmente_positivi"])
+df_reg = df_reg.filter(["data", "denominazione_regione", "totale_casi", "terapia_intensiva", "nuovi_attualmente_positivi", "deceduti", "totale_attualmente_positivi", "tamponi"])
 df_reg["day"] = -1
 
 frames = [df_it, df_reg]
@@ -65,22 +72,28 @@ for region in FILTERED_REGIONS:
   # regions_data[region].index = range(regions_data[region].size)
 
   regions_data[region]["totale_casi_ieri"] = regions_data[region]["totale_casi"].shift(1)
+
   today = regions_data[region]["totale_casi"]
   yesterday = regions_data[region]["totale_casi_ieri"]
-  # regions_data[region]["delta_totale"] = (today - yesterday) / yesterday * 100
   regions_data[region]["delta_totale"] = (today - yesterday)
+
+  if ONLY_SINGLE_ONE:
+    regions_data[region]["tamponi_ieri"] = regions_data[region]["tamponi"].shift(1)
+    today = regions_data[region]["tamponi"]
+    yesterday = regions_data[region]["tamponi_ieri"]
+    regions_data[region]["delta_tamponi"] = (today - yesterday)
 
   regions_data[region]["terapia_intensiva_ieri"] = regions_data[region]["terapia_intensiva"].shift(1)
   today = regions_data[region]["terapia_intensiva"]
   yesterday = regions_data[region]["terapia_intensiva_ieri"]
-  # regions_data[region]["delta_terapia_intensiva"] = (today - yesterday) / yesterday * 100
   regions_data[region]["delta_terapia_intensiva"] = (today - yesterday)
 
-  regions_data[region]["deceduti_ieri"] = regions_data[region]["deceduti"].shift(1)
-  today = regions_data[region]["deceduti"]
-  yesterday = regions_data[region]["deceduti_ieri"]
-  # regions_data[region]["delta_deceduti"] = (today - yesterday) / yesterday * 100
-  regions_data[region]["delta_deceduti"] = (today - yesterday)
+  regions_data[region]["totale_deceduti_ieri"] = regions_data[region]["deceduti"].shift(1)
+  regions_data[region]["deceduti_oggi"] = regions_data[region]["deceduti"] - regions_data[region]["totale_deceduti_ieri"]
+  regions_data[region]["deceduti_ieri"] = regions_data[region]["deceduti_oggi"].shift(1)
+  regions_data[region]["delta_deceduti"] = regions_data[region]["deceduti_oggi"] - regions_data[region]["deceduti_ieri"]
+
+  regions_data[region]["deceduti_oggi"] = (today - yesterday)
 
   regions_list.append(regions_data[region])
 
@@ -96,6 +109,8 @@ for region in FILTERED_REGIONS:
   axs[0, 0].plot(X, "totale_casi", label=region, data=regions_data[region], markersize=2, color=getColor(region), linewidth=2)
   # axs[0, 0].plot(X, "totale_attualmente_positivi", label=region, data=regions_data[region], marker='o', markersize=2, color=getColor(region), linewidth=0)
   axs[0, 1].plot(X, "delta_totale", label=region, data=regions_data[region], markersize=2, color=getColor(region), linewidth=2)
+  if ONLY_SINGLE_ONE:
+    axs[0, 1].plot(X, "delta_tamponi", label=region, data=regions_data[region], markersize=2, color="red", linewidth=2)
 
   axs[1, 0].plot(X, "terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=getColor(region), linewidth=2)
   axs[1, 1].plot(X, "delta_terapia_intensiva", label=region, data=regions_data[region], markersize=2, color=getColor(region), linewidth=2)
@@ -105,12 +120,15 @@ for region in FILTERED_REGIONS:
 
 
 axs[0, 0].set_title('Totale casi')
-axs[0, 1].set_title('Aumento casi su giorno precedente')
+if ONLY_SINGLE_ONE:
+  axs[0, 1].set_title('Aumento casi su giorno precedente / Tamponi')
+else:
+  axs[0, 1].set_title('Aumento casi su giorno precedente')
 
 axs[1, 0].set_title('Terapia intensiva')
 axs[1, 1].set_title('Aumento TI su giorno precedente')
 
-axs[2, 0].set_title('Deceduti')
+axs[2, 0].set_title('Totale Deceduti')
 axs[2, 1].set_title('Aumento deceduti su giorno precedente')
 
 axs[2, 1].legend(loc='upper center', bbox_to_anchor=(0, -0.2), ncol=5, fancybox=True, shadow=True)
