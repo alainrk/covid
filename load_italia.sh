a5571c7a6b33a516a5bb40ce63028a85d58d1ce3
Alain Di Chiappari <alain.devcs@gmail.com>
2020-04-30 15:59:42 +0200

    data

--- load_italia.sh
+++ load_italia.sh
@@ -11,3 +11,5 @@ for i in $(seq 10 31); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-1
 for i in $(seq 1 9); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-2020040$i.csv | tail -n +2 >> italia.csv; done
 
 for i in $(seq 10 30); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-202004$i.csv | tail -n +2 >> italia.csv; done
+
+for i in $(seq 1 9); do curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-2020050$i.csv | tail -n +2 >> italia.csv; done
