68ade5a8664a6ad80ba65e9f7960e84a823b4c00
Alain Di Chiappari <alain.devcs@gmail.com>
2020-06-01 06:15:28 +0200

    data

--- load.sh
+++ load.sh
@@ -10,8 +10,10 @@ fi
 # ./load_italia.sh &&
 # ./load_regioni.sh
 
-i=12
-curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-202005$i.csv | tail -n +2 >> italia.csv
-curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202005$i.csv | tail -n +2 >> regioni.csv
+i=1
+curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-2020060$i.csv | tail -n +2 >> italia.csv
+curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-2020060$i.csv | tail -n +2 >> regioni.csv
+# curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-202005$i.csv | tail -n +2 >> italia.csv
+# curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202005$i.csv | tail -n +2 >> regioni.csv
 
 exit 0
